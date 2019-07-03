ebae52ba538deb9d353bfc5fa23e9166fdb2a739
Sanjay Verma <sanjav@amazon.com>
2019-07-02 19:53:43 -0700

    Add Rest base URL injection

--- projects/ix-angular-api/src/lib/api/api.service.ts
+++ projects/ix-angular-api/src/lib/api/api.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, InjectionToken } from '@angular/core';
 import { Observable } from 'rxjs';
 import { API } from './api';
+
+export const URL_TOKEN = new InjectionToken<string>('API_BASE_URL');
 
 @Injectable({
   providedIn: 'root'
 })
 export class APIService {
-  constructor(private http: HttpClient, private URL: string) { }
+  constructor(private http: HttpClient, @Inject(URL_TOKEN) private URL: string) { }
 
   public call(api: API): Observable<any> {
     return api.call(this.http, this.URL);
