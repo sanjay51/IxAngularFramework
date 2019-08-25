049d25915a14c41da2a4be7a6eaa162bec3789b4
Sanjay Verma <sanjayvermaseattle@gmail.com>
2019-08-25 09:30:31 -0700

    Make IxCard members public

--- projects/ix-angular-elements/src/lib/elements/ix-icon-box/IxCard.ts
+++ projects/ix-angular-elements/src/lib/elements/ix-icon-box/IxCard.ts
@@ -1,8 +1,8 @@
 export class IxCard {
   constructor(
-    private icon: string,
-    private title: string,
-    private description: string,
+    public icon: string,
+    public title: string,
+    public description: string,
     public link: string,
     public backgroundColor: string,
     public titleColor: string
